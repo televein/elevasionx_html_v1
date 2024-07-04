29858b4783e750c119a3a6a45d82dc0a2f585c57
GOKULA KANNAN V <gokulakannanv04@gmail.com>
2024-07-04 15:25:58 +0530

    Automated commit - 2024-07-04 15:25:58

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        keyframes: {
+          scroll: {
+            '0%': { transform: 'translateX(0)' },
+            '100%': { transform: 'translateX(-50%)' },
+          },
+        },
+        animation: {
+          scroll: 'scroll 20s linear infinite',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
